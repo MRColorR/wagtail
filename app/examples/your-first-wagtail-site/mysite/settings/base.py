97f721db64937e0cb91362c17f9bd3fa8d4c3b5c
MRColorR <72873425+MRColorR@users.noreply.github.com>
2025-07-07 15:48:27 +0200

    feat(example): enable heading anchors in wagtail-markdown with additional extensions

--- app/examples/your-first-wagtail-site/mysite/settings/base.py
+++ app/examples/your-first-wagtail-site/mysite/settings/base.py
@@ -186,3 +186,11 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+# Enable heading anchors in wagtail-markdown
+WAGTAILMARKDOWN = {
+    "extensions": [
+        "markdown.extensions.toc",
+        "markdown.extensions.attr_list",
+    ]
+}
